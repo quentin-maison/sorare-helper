ef41ed0092a1197a352470841da0b3f761c9fb13
quentin-maison <quentin.maison.92@gmail.com>
2021-10-12 11:20:19 +0200

    testing api sorare-helper

---
+++ sorare-helper-api/index.js
@@ -0,0 +1,20 @@
+
+const express = require('express')
+const app = express()
+const axios = require('axios').default;
+const port = 3001
+
+app.get('/*', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  axios.get('https://api.sorare.com' + req.path).then((response) => {
+    res.set('Access-Control-Allow-Origin', '*')
+    res.json(response.data)
+  })
+  
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
+
+
--- src/App.js
+++ src/App.js
@@ -1,7 +1,55 @@
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  
+
+
+      const urlAPI = 'https://api.sorare.com/api/v1/users/m_quentin_m@hotmail.fr'
+      const urlProxy= 'http://localhost:3001'
+
+      let headers = new Headers();
+      headers.append('Access-Control-Allow-Origin', 'http://localhost:3000');
+
+      //TEST QUENTIN
+
+      fetch(`${urlProxy}/${urlAPI}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: headers
+      }).then(
+        (response) => {
+          console.log(response)
+        }
+      ).catch(
+        (error) => {
+          console.log(error)
+        }
+      )
+      
+      //ANCIENNE VERSION
+
+      fetch(urlAPI, {
+        method: 'GET',
+        mode: 'cors',
+        headers: headers
+      }).then(
+        async (response) => {
+          try {
+            const responseJSON = await response.json();
+            console.log(responseJSON);
+          } catch (error) {
+            console.log(error);
+          }
+        }
+      ).catch(
+        (error) => {
+          console.log(error)
+        }
+      )
+  
+  
   return (
     <div className="App">
       <header className="App-header">
