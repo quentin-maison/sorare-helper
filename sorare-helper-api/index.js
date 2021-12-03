276090c86385cef898817ff3f911a6ce71abf534
quentin-maison <quentin.maison.92@gmail.com>
2021-12-03 19:49:56 +0100

    api update

--- sorare-helper-api/index.js
+++ sorare-helper-api/index.js
@@ -1,28 +1,47 @@
-
 const express = require('express')
 const app = express()
 const axios = require('axios').default;
 const port = 3001
+var bodyParser = require('body-parser')
+app.use(bodyParser.json())
+var cors = require('cors')
+app.use(cors())
 
-app.get('/*', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  axios.get('https://api.sorare.com' + req.path).then((response) => {
-    res.set('Access-Control-Allow-Origin', '*')
-    res.json(response.data)
-  })
-  
+const fs = require('fs')
+let dataManagers = undefined
+fs.readFile('./10000_save_managers.json', 'utf8' , (err, data) => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  dataManagers = JSON.parse(data)
+
 })
+
 app.post('/*', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  axios.post('https://api.sorare.com' + req.path, req.body).then((response) => {
+  axios.post('https://api.sorare.com' + req.path, req.body, {
+    headers: {
+      'Content-Type': 'application/json'
+  }}).then((response) => {
     res.set('Access-Control-Allow-Origin', '*')
     res.json(response.data)
+  }).catch((e) => {
+    res.set('Access-Control-Allow-Origin', '*')
+    res.json(response.json())
   })
+
   
 })
+
+
+app.get('/search/:key', (req, res) => {
+  const manager = dataManagers.find((managerObject) => managerObject.manager.Nickname.toLowerCase().includes(req.params.key.toLowerCase()))
+  res.json(JSON.stringify(manager))
+})
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
